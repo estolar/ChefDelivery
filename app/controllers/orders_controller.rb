9e568df7aaf2d2ac30d7f488e4a0a284fec21e65
Pamela Matias <pamelamc23@gmail.com>
2024-09-05 01:44:57 -0500

    Modificacion new-service

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,42 @@
 class OrdersController < ApplicationController
+
+  def index
+
+  end
+
   def show
 
   end
 
-  def create
+  def new
+    @order = Order.new
   end
 
-  def new
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:service_id, :user_id)
+  end
+
 end
